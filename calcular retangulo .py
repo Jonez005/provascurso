55e419c1e05dcbafdd31232fbef8f4656cb045a0
Jonez005 <kevinjonesdamasceno@gmail.com>
2025-03-04 23:25:04 -0300

    Add files via upload

---
+++ calcular retangulo .py
@@ -0,0 +1,11 @@
+#Area e perimetro de um retângulo 
+#Primeria prova#
+
+largura = float(input('entre com a largura'))
+altura = float(input('entre com a altura'))
+
+area = largura * altura
+perimetro = 2 * (largura + altura)
+
+print(f'A area do retangulo e: {area}')
+print(f'O perimetro do retangulo e: {perimetro}')
